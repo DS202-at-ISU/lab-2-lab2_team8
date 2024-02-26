7e923626483a7c988f59735218970cd94b1c509b
Ashraf Shaikh Mohammed <ashraf@Ashrafs-MacBook-Pro.local>
2024-02-25 23:33:49 -0600

    Final exploration with scatterplots

--- exploration_workspace.R
+++ exploration_workspace.R
@@ -46,13 +46,13 @@ ggplot(data = filter(ames, !is.na(Occupancy)), aes(Occupancy)) + geom_bar()
 # Also, creates NA as a category when works
 # try filter
 ggplot(data = filter(ames, !is.na(`Multi Sale`)), aes(factor(`Multi Sale`))) + geom_bar()
-ggplot(data = filter(ames, !is.na(`Multi Sale`)), aes(`Multi Sale`)) + geom_bar()
+#ggplot(data = filter(ames, !is.na(`Multi Sale`)), aes(`Multi Sale`)) + geom_bar()
 # no difference between 2 above cuz only Y, no N
 
-ggplot(data = ames, aes(factor(Bedrooms))) + geom_bar()
+#ggplot(data = ames, aes(factor(Bedrooms))) + geom_bar()
 # Has 0 bedrooms and NA as category so use filter for removing NA values
 # You may consider excluding 0 because 0 bedrooms might not make sense
-ggplot(data = filter(ames, !is.na(Bedrooms)), aes(factor(Bedrooms))) + geom_bar()
+#ggplot(data = filter(ames, !is.na(Bedrooms)), aes(factor(Bedrooms))) + geom_bar()
 ggplot(data = filter(ames, !is.na(Bedrooms)), aes(factor(Bedrooms, exclude = 0))) + geom_bar()
 
 ggplot(data = ames, aes(factor(AC))) + geom_bar()
@@ -63,7 +63,7 @@ ggplot(data = ames, aes(factor(FirePlace))) + geom_bar()
 # I dont think FirePlace has NA, use below if has NA
 # ggplot(data = filter(ames, !is.na(FirePlace)), aes(factor(FirePlace))) + geom_bar()
 
-ggplot(data = ames, aes(Neighborhood)) + geom_bar() + coord_flip()
+# ggplot(data = ames, aes(Neighborhood)) + geom_bar() + coord_flip()
 # what's the deal with 0 none type neighborhood?
 ggplot(data = filter(ames, Neighborhood != "(0) None"), aes(Neighborhood)) + geom_bar() + coord_flip()
 
@@ -98,26 +98,126 @@ ggplot(data = filter(ames, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtAre
 ggplot(data = filter(ames, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_histogram(binwidth = 100)
 
 ####################################
+# Let's try to plot some boxplots for QUANTITATIVE
+####################################
+
+# remember to ignore NAs for categorical when filling
+ames_clean_Style <- filter(ames, !is.na(Style))
+ames_clean_Occupancy <- filter(ames, !is.na(Occupancy))
+ames_clean_Bedrooms <- filter(ames, !is.na(Bedrooms) & Bedrooms != 0)
+ames_clean_Neighborhood <- filter(ames, Neighborhood != "(0) None")
+# instead, can also do general like below (but it'll have few rows, above style is for if we only care about particular categorical)
+#ames_clean <- filter(ames, !is.na(Style) & !is.na(Occupancy) & !is.na(Bedrooms) & Bedrooms != 0 & Neighborhood != "(0) None")
+
+ggplot(data = ames, aes(`Sale Date`)) + geom_boxplot()
+
+ggplot(data = filter(ames_clean_Style, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_boxplot() + facet_wrap(~Style)
+ggplot(data = filter(ames_clean_Occupancy, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_boxplot() + facet_wrap(~Occupancy)
+ggplot(data = filter(ames_clean_Bedrooms, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_boxplot() + facet_wrap(~factor(Bedrooms))
+ggplot(data = filter(ames, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_boxplot() + facet_wrap(~factor(AC))
+ggplot(data = filter(ames, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_boxplot() + facet_wrap(~factor(FirePlace))
+ggplot(data = filter(ames_clean_Neighborhood, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_boxplot() + facet_wrap(~Neighborhood)
+
+ggplot(data = filter(ames, !is.na(YearBuilt) & YearBuilt > 1850), aes(YearBuilt)) + geom_boxplot()
+
+ggplot(data = filter(ames, !is.na(Acres) & Acres > 2.5 & Acres < 5), aes(Acres)) + geom_boxplot()
+ggplot(data = filter(ames, !is.na(Acres) & Acres > 1 & Acres <= 2.5), aes(Acres)) + geom_boxplot()
+ggplot(data = filter(ames, !is.na(Acres) & Acres > 0 & Acres <= 1), aes(Acres)) + geom_boxplot()
+
+ggplot(data = filter(ames, `TotalLivingArea (sf)` >= 3000), aes(`TotalLivingArea (sf)`)) + geom_boxplot()
+ggplot(data = filter(ames, `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000), aes(`TotalLivingArea (sf)`)) + geom_boxplot()
+
+ggplot(data = filter(ames, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtArea (sf)`)) + geom_boxplot()
+
+ggplot(data = filter(ames, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_boxplot()
+
+####################################
 # Let's try to plot some histograms for QUANTITATIVE with CATEGORICAL fills to see trends
 ####################################
 # CAN TRY CATEGORICAL WITH OTHER CATEGORICAL FILLS BUT MAY NOT BE USEFUL SINCE MAIN VARIABLE IS QUANTITATIVE
+# like ggplot based on one categorical and geom bar fill(weight) based on other
+# or like ggplot based on one categorical and facet wrap using one categorical and geombar(fill with other categorical) this will give 3 categorical comparison
 
+ggplot(data = ames_clean_Style, aes(`Sale Date`)) + geom_histogram(aes(fill = Style))
+ggplot(data = ames_clean_Occupancy, aes(`Sale Date`)) + geom_histogram(aes(fill = Occupancy))
+ggplot(data = ames_clean_Bedrooms, aes(`Sale Date`)) + geom_histogram(aes(fill = factor(Bedrooms)))
+ggplot(data = ames, aes(`Sale Date`)) + geom_histogram(aes(fill = factor(AC)))
+ggplot(data = ames, aes(`Sale Date`)) + geom_histogram(aes(fill = factor(FirePlace)))
+ggplot(data = ames_clean_Neighborhood, aes(`Sale Date`)) + geom_histogram(aes(fill = Neighborhood))
 
-ggplot(data = ames, aes(`Sale Date`)) + geom_histogram()
+ggplot(data = filter(ames_clean_Style, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_histogram(binwidth = 10000, aes(fill = Style))
+ggplot(data = filter(ames_clean_Occupancy, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_histogram(binwidth = 10000, aes(fill = Occupancy))
+ggplot(data = filter(ames_clean_Bedrooms, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_histogram(binwidth = 10000, aes(fill = factor(Bedrooms)))
+ggplot(data = filter(ames, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_histogram(binwidth = 10000, aes(fill = factor(AC)))
+ggplot(data = filter(ames, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_histogram(binwidth = 10000, aes(fill = factor(FirePlace)))
+ggplot(data = filter(ames_clean_Neighborhood, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_histogram(binwidth = 10000, aes(fill = Neighborhood))
 
-ggplot(data = filter(ames, `Sale Price` > 10000 & `Sale Price` <= 1000000), aes(`Sale Price`)) + geom_histogram(binwidth = 10000)
+ggplot(data = filter(ames_clean_Style, !is.na(YearBuilt) & YearBuilt > 1850), aes(YearBuilt)) + geom_histogram(binwidth = 1, aes(fill = Style))
+ggplot(data = filter(ames_clean_Occupancy, !is.na(YearBuilt) & YearBuilt > 1850), aes(YearBuilt)) + geom_histogram(binwidth = 1, aes(fill = Occupancy))
+ggplot(data = filter(ames_clean_Bedrooms, !is.na(YearBuilt) & YearBuilt > 1850), aes(YearBuilt)) + geom_histogram(binwidth = 1, aes(fill = factor(Bedrooms)))
+ggplot(data = filter(ames, !is.na(YearBuilt) & YearBuilt > 1850), aes(YearBuilt)) + geom_histogram(binwidth = 1, aes(fill = factor(AC)))
+ggplot(data = filter(ames, !is.na(YearBuilt) & YearBuilt > 1850), aes(YearBuilt)) + geom_histogram(binwidth = 1, aes(fill = factor(FirePlace)))
+ggplot(data = filter(ames_clean_Neighborhood, !is.na(YearBuilt) & YearBuilt > 1850), aes(YearBuilt)) + geom_histogram(binwidth = 1, aes(fill = Neighborhood))
 
-ggplot(data = filter(ames, !is.na(YearBuilt) & YearBuilt > 1850), aes(YearBuilt)) + geom_histogram(binwidth = 1)
+ames_AcresTypes <- filter(ames, !is.na(Acres) & Acres > 0)
+ames_AcresTypes$AcresType <- with(ames_AcresTypes, ifelse(Acres > 2.5 & Acres < 5,'L',
+                                                          ifelse(Acres > 1 & Acres <= 2.5, 'M', 'S')))
+ames_AT_clean_Style <- filter(ames_AcresTypes, !is.na(Style))
+ames_AT_clean_Occupancy <- filter(ames_AcresTypes, !is.na(Occupancy))
+ames_AT_clean_Bedrooms <- filter(ames_AcresTypes, !is.na(Bedrooms) & Bedrooms != 0)
+ames_AT_clean_Neighborhood <- filter(ames_AcresTypes, Neighborhood != "(0) None")
+ggplot(data = ames_AT_clean_Style, aes(Acres)) + facet_wrap(~factor(AcresType), scales = "free") + geom_histogram(aes(fill = Style))
+ggplot(data = ames_AT_clean_Occupancy, aes(Acres)) + facet_wrap(~factor(AcresType), scales = "free") + geom_histogram(aes(fill = Occupancy))
+ggplot(data = ames_AT_clean_Bedrooms, aes(Acres)) + facet_wrap(~factor(AcresType), scales = "free") + geom_histogram(aes(fill = factor(Bedrooms)))
+ggplot(data = ames_AcresTypes, aes(Acres)) + facet_wrap(~factor(AcresType), scales = "free") + geom_histogram(aes(fill = factor(AC)))
+ggplot(data = ames_AcresTypes, aes(Acres)) + facet_wrap(~factor(AcresType), scales = "free") + geom_histogram(aes(fill = factor(FirePlace)))
+ggplot(data = ames_AT_clean_Neighborhood, aes(Acres)) + facet_wrap(~factor(AcresType), scales = "free") + geom_histogram(aes(fill = Neighborhood))
 
-ggplot(data = filter(ames, !is.na(Acres) & Acres > 2.5 & Acres < 5), aes(Acres)) + geom_histogram(binwidth = 0.025)
-ggplot(data = filter(ames, !is.na(Acres) & Acres > 1 & Acres <= 2.5), aes(Acres)) + geom_histogram(binwidth = 0.01)
-ggplot(data = filter(ames, !is.na(Acres) & Acres > 0 & Acres <= 1), aes(Acres)) + geom_histogram(binwidth = 0.005)
-
-ggplot(data = filter(ames, `TotalLivingArea (sf)` >= 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25)
+ggplot(data = filter(ames, `TotalLivingArea (sf)` >= 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25)# dont care about this for now
 ggplot(data = filter(ames, `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25)
+ggplot(data = filter(ames_clean_Style, `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25, aes(fill = Style))
+ggplot(data = filter(ames_clean_Occupancy, `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25, aes(fill = Occupancy))
+ggplot(data = filter(ames_clean_Bedrooms, `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25, aes(fill = factor(Bedrooms)))
+ggplot(data = filter(ames, `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25, aes(fill = factor(AC)))
+ggplot(data = filter(ames, `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25, aes(fill = factor(FirePlace)))
+ggplot(data = filter(ames_clean_Neighborhood, `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000), aes(`TotalLivingArea (sf)`)) + geom_histogram(binwidth = 25, aes(fill = Neighborhood))
 
-ggplot(data = filter(ames, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtArea (sf)`)) + geom_histogram(binwidth = 10)
+ggplot(data = filter(ames_clean_Style, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtArea (sf)`)) + geom_histogram(binwidth = 10, aes(fill = Style))
+ggplot(data = filter(ames_clean_Occupancy, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtArea (sf)`)) + geom_histogram(binwidth = 10, aes(fill = Occupancy))
+ggplot(data = filter(ames_clean_Bedrooms, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtArea (sf)`)) + geom_histogram(binwidth = 10, aes(fill = factor(Bedrooms)))
+ggplot(data = filter(ames, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtArea (sf)`)) + geom_histogram(binwidth = 10, aes(fill = factor(AC)))
+ggplot(data = filter(ames, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtArea (sf)`)) + geom_histogram(binwidth = 10, aes(fill = factor(FirePlace)))
+ggplot(data = filter(ames_clean_Neighborhood, `FinishedBsmtArea (sf)` < 2000), aes(`FinishedBsmtArea (sf)`)) + geom_histogram(binwidth = 10, aes(fill = Neighborhood))
 
-ggplot(data = filter(ames, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_histogram(binwidth = 100)
+ggplot(data = filter(ames_clean_Style, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_histogram(binwidth = 100, aes(fill = Style))
+ggplot(data = filter(ames_clean_Occupancy, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_histogram(binwidth = 100, aes(fill = Occupancy))
+ggplot(data = filter(ames_clean_Bedrooms, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_histogram(binwidth = 100, aes(fill = factor(Bedrooms)))
+ggplot(data = filter(ames, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_histogram(binwidth = 100, aes(fill = factor(AC)))
+ggplot(data = filter(ames, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_histogram(binwidth = 100, aes(fill = factor(FirePlace)))
+ggplot(data = filter(ames_clean_Neighborhood, !is.na(`LotArea(sf)`) & `LotArea(sf)` > 100 & `LotArea(sf)` < 15000), aes(`LotArea(sf)`)) + geom_histogram(binwidth = 100, aes(fill = Neighborhood))
+
+####################################
+# Let's try to plot some scatterplots for QUANTITATIVE with QUANTITATIVE, specifically sale price, may use fill with CATEGORICAL
+####################################
+
+# play around with acres, living area, salesprice, etc ranges to find oddities
+# filter doesn't like filter(data = ames, ...), it only needs name of dataframe, no need data = , like filter(ames, ...)
+ames_clean_scatterplot <- filter(ames, `Sale Price` > 10000 & `Sale Price` <= 1000000
+                                     , !is.na(YearBuilt) & YearBuilt > 1850
+                                     , !is.na(Acres) & Acres > 0 & Acres < 5
+                                     , `TotalLivingArea (sf)` > 25 & `TotalLivingArea (sf)` < 3000
+                                     #, `FinishedBsmtArea (sf)` < 2000
+                                     , !is.na(`LotArea(sf)`) #& `LotArea(sf)` > 100 & `LotArea(sf)` < 15000
+                                     , !is.na(Style) & !is.na(Occupancy) & !is.na(Bedrooms) & Bedrooms != 0 & Neighborhood != "(0) None")
+
+ggplot(data = ames_clean_scatterplot, aes(x = YearBuilt, y = `Sale Price`)) + geom_point()
+
+ggplot(data = ames_clean_scatterplot, aes(x = Acres, y = `Sale Price`)) + geom_point()
+
+ggplot(data = ames_clean_scatterplot, aes(x = `TotalLivingArea (sf)`, y = `Sale Price`)) + geom_point()
+
+ggplot(data = ames_clean_scatterplot, aes(x = `FinishedBsmtArea (sf)`, y = `Sale Price`)) + geom_point()
+
+ggplot(data = ames_clean_scatterplot, aes(x = `LotArea(sf)`, y = `Sale Price`)) + geom_point()
 
 
